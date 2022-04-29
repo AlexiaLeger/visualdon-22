dbf63214d2929423ef073013b69ae8948c9c03ce
Alexia Leger <alexia.leger@heig-vd.ch>
2022-04-29 11:16:12 +0200

    Exercice terminé

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
-import * as puppeteer from "puppeteer";
+import puppeteer from "puppeteer";
+import { JSDOM } from 'jsdom';
+import fetch from "isomorphic-fetch"
 
 (async () => {
     // Lancement browser
@@ -11,27 +12,43 @@ import * as puppeteer from "puppeteer";
     await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
 
     //Faire un screenshot
-    const screen = await page.screenshot({ path: 'cantons.png', fullPage:true });
+    const screen = await page.screenshot({ path: '05-web-scraping\\cantons.png', fullPage: false });
     console.log(screen);
-
-    //Webscraper wikipedia
-    //Sélecteurs
-    //await page.$(table.wikitable td); // document.querySelector
-    //await page.$$(sélecteur); // document.querySelectorAll
-    // EXEMPLE : await page.$$(div);
-     
-    //Appliquer une fonction aux sélecteurs
-    //await page.$$eval(sélecteur, pageFunction[...args]);
-    // EXEMPLE : const divCount = await page.$$eval('div', (divs) => divs.length);
 
     // Fermer les browser
     await browser.close();
 })();
 
-//webscraper site e-commerce
-=======
-import jsdom from "jsdom";
-import fetch from "isomorphic-fetch"
-import puppeteer from "puppeteer"
+//recupérer les cantons
+(async () => {
+    const response = await fetch('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
+    const text = await response.text();
+    const dom = new JSDOM(text);
+    const cantons = dom.window.document.querySelectorAll("table.wikitable tr");
+    for (const canton of cantons) {
+        try {
+            const nom = canton.querySelector("td a").textContent;
+            const pop = canton.querySelector("td bdi").textContent;
+            //console.log("Cantons : " + nom + ", Population : " + pop);
+        } catch (error) { }
+    }
+})();
 
->>>>>>> 33002473cc1fe9ac1760f63518f801ae49732448
+
+//webscraper site e-commerce
+(async () => {
+    const response = await fetch('https://www.webscraper.io/test-sites/e-commerce/allinone/computers/laptops');
+    const text = await response.text();
+    const dom = new JSDOM(text);
+    const produits = dom.window.document.querySelectorAll("div.thumbnail ");
+    for (const produit of produits) {
+        try {
+            const ligne = {
+                "produit" : produit.querySelector("div.caption h4 a").textContent,
+                "prix" : produit.querySelector("div.caption h4").textContent,
+                "etoiles" : produit.querySelectorAll("div.ratings p span").length
+            }
+            console.log(ligne);
+        } catch (error) {}   
+    }
+})()
