1b85ff7c6926e40defc546e45020b991e165c19c
Alexia Leger <alexia.leger@heig-vd.ch>
2022-04-29 08:27:18 +0200

    Commencer exercice

--- 05-web-scraping/src/index.js
+++ 05-web-scraping/src/index.js
@@ -1,1 +1,30 @@
 import * as puppeteer from "puppeteer";
+
+(async () => {
+    // Lancement browser
+    const browser = await puppeteer.launch();
+    //Ouvrir une nouvelle page
+    const page = await browser.newPage();
+
+    //Aller au lien wikipedia
+    await page.goto('https://fr.wikipedia.org/wiki/Canton_(Suisse)#Donn%C3%A9es_cantonales');
+
+    //Faire un screenshot
+    const screen = await page.screenshot({ path: 'cantons.png', fullPage:true });
+    console.log(screen);
+
+    //Webscraper wikipedia
+    //Sélecteurs
+    //await page.$(table.wikitable td); // document.querySelector
+    //await page.$$(sélecteur); // document.querySelectorAll
+    // EXEMPLE : await page.$$(div);
+     
+    //Appliquer une fonction aux sélecteurs
+    //await page.$$eval(sélecteur, pageFunction[...args]);
+    // EXEMPLE : const divCount = await page.$$eval('div', (divs) => divs.length);
+
+    // Fermer les browser
+    await browser.close();
+})();
+
+//webscraper site e-commerce
